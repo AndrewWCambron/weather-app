aa30ada228ae789a9e46e34e86eb1b5c005c324a
Andrew Cambron <andrew.william.cambron@gmail.com>
2023-11-22 11:31:40 -0500

    weather app

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "14c88526d1msh00ccbaf1466d530p10df62jsnc74759372ad3",// enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "63ca547501b76b7f771adcb7e8b455b1"; // enter your key from openweather API
